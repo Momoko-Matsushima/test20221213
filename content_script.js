32293b4440cb86f08474e3c4cf7cf9a3b0829765
2140805h <2140805h@gsuite.kobe-u.ac.jp>
2022-12-13 11:29:35 +0900

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -3,3 +3,17 @@ for(let i = 0; i < calendars.length; i++){
   let e = calendars[i];
   e.style.display = 'none';
 }
+function f(n){
+    let cs = n.childNodes;
+    for(let i = 0; i < cs.length; i++){
+      let c = cs[i];
+      if(c.nodeType == Node.TEXT_NODE){
+        c.textContent = c.textContent.replace(/です。/g, "でござるよ。");
+      }
+      else{
+        f(c);
+      }
+    }
+  }
+  
+  f(document.body); // document.body は HTML の文章全体を示す要素
