9aa441e44c7c24959649306adb376778afc5b40c
2140805h <2140805h@gsuite.kobe-u.ac.jp>
2022-12-20 10:37:10 +0900

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,15 +1,17 @@
-let calendars = document.querySelectorAll(".list-rst__calendar-frame");
+let calendars = document.querySelectorAll(".p-booking-calender");
 for(let i = 0; i < calendars.length; i++){
   let e = calendars[i];
   e.style.display = 'none';
 }
+
+
 
 function f(n){
     let cs = n.childNodes;
     for(let i = 0; i < cs.length; i++){
       let c = cs[i];
       if(c.nodeType == Node.TEXT_NODE){
-        c.textContent = c.textContent.replace(/東京/g, "🗼").replace(/大阪/g,"🏃");
+        c.textContent = c.textContent.replace(/ます。/g, "ます🗼").replace(/大阪/g,"🏃");
       }
       else{
         f(c);
