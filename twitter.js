4c7196935e81a66125347f7000cea1b86d0132a9
2140805h <2140805h@gsuite.kobe-u.ac.jp>
2023-01-23 19:08:44 +0900

    Create twitter.js

---
+++ twitter.js
@@ -0,0 +1,25 @@
+
+
+let oReactions = document.querySelectorAll(".r-q3we1");
+for(let i = 0; i < oReactions.length; i++){
+  let e = reactions[i];
+  e.style.display = 'none';
+}
+
+let reactions = document.querySelectorAll(".r-xoduu5");
+for(let i = 0; i < reactions.length; i++){
+  let e = reactions[i];
+  e.style.display = 'none';
+}
+
+let icons = document.querySelectorAll(".r-1hwvwag");
+for(let i = 0; i < icons.length; i++){
+  let e = icons[i];
+  e.style.display = 'none';
+}
+
+let names = document.querySelectorAll(".r-zl2h9q");
+for(let i = 0; i < names.length; i++){
+  let e = names[i];
+  e.style.display = 'none';
+}
